1742b5b27156f92f8c67ffff12e244ab45fb9d82
Ralph Sliger <ralph.sliger@prodigio.tech>
2023-07-23 11:15:45 -0500

    get all tickets

--- domain/usecase/src/main/java/co/com/doublev/usecase/getalltickets/GetAllTicketsUseCase.java
+++ domain/usecase/src/main/java/co/com/doublev/usecase/getalltickets/GetAllTicketsUseCase.java
@@ -1,6 +1,17 @@
 package co.com.doublev.usecase.getalltickets;
 
+import co.com.doublev.model.ticket.Ticket;
+import co.com.doublev.model.ticket.gateways.TicketRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 public class GetAllTicketsUseCase {
+
+    private final TicketRepository ticketRepository;
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.getAllTickets();
+    }
 }
