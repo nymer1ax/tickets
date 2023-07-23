a823e11a17274f65e7197f08f8da51fd30af1af7
Ralph Sliger <ralph.sliger@prodigio.tech>
2023-07-23 10:09:19 -0500

    Initial commit

---
+++ applications/app-service/src/main/java/co/com/doublev/MainApplication.java
@@ -0,0 +1,11 @@
+package co.com.doublev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
