04441aebef075d42323207195e46aec389978404
Ralph Sliger <ralph.sliger@prodigio.tech>
2023-07-23 11:01:59 -0500

    create new ticket use case

--- domain/usecase/src/main/java/co/com/doublev/usecase/createticket/CreateTicketUseCase.java
+++ domain/usecase/src/main/java/co/com/doublev/usecase/createticket/CreateTicketUseCase.java
@@ -1,6 +1,14 @@
 package co.com.doublev.usecase.createticket;
 
+import co.com.doublev.model.ticket.Ticket;
+import co.com.doublev.model.ticket.gateways.TicketRepository;
 import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CreateTicketUseCase {
+
+    private final TicketRepository ticketRepository;
+    public Ticket createNewTicket(Ticket ticket){
+       return ticketRepository.createTicket(ticket);
+    }
+
 }
