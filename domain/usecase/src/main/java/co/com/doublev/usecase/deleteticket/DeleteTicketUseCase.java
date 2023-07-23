a69b6ee326eda85e91928c81087650081317db48
Ralph Sliger <ralph.sliger@prodigio.tech>
2023-07-23 11:12:02 -0500

    delete ticket

--- domain/usecase/src/main/java/co/com/doublev/usecase/deleteticket/DeleteTicketUseCase.java
+++ domain/usecase/src/main/java/co/com/doublev/usecase/deleteticket/DeleteTicketUseCase.java
@@ -1,6 +1,26 @@
 package co.com.doublev.usecase.deleteticket;
 
+import co.com.doublev.model.ticket.Ticket;
+import co.com.doublev.model.ticket.gateways.TicketRepository;
+import co.com.doublev.usecase.exceptions.custom.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
 @RequiredArgsConstructor
 public class DeleteTicketUseCase {
+
+    private final TicketRepository ticketRepository;
+
+    public void deleteTicket(Long ticketId) {
+
+        Optional<Ticket> ticketExist = ticketRepository.getTicketById(ticketId);
+
+        if (ticketExist.isEmpty()) {
+            throw new ResourceNotFoundException("Ticket could not be delete.");
+        }
+
+        ticketRepository.deleteTicket(ticketId);
+
+    }
 }
